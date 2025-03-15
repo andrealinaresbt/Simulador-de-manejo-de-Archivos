579695d8521c37b1fe93efac0690a293d2439571
Andrea Linares <linares.andrea@correo.unimet.edu.ve>
2025-03-14 20:49:47 -0400

    :heart: Modifications

--- src/main/java/Almacenamiento/Disco.java
+++ src/main/java/Almacenamiento/Disco.java
@@ -9,13 +9,14 @@ package Almacenamiento;
  * @author Andrea
  */
 public class Disco {
-
     private Bloque[] bloques;
     private boolean[] ocupado;
+    private int bloquesLibres;
 
     public Disco(int totalBloques) {
         this.bloques = new Bloque[totalBloques];
         this.ocupado = new boolean[totalBloques];
+        this.bloquesLibres = totalBloques;
 
         for (int i = 0; i < totalBloques; i++) {
             bloques[i] = new Bloque(i);
@@ -24,6 +25,11 @@ public class Disco {
     }
 
     public int asignarBloques(int cantidad) {
+        if (cantidad > bloquesLibres) {
+            System.out.println("Error: No hay suficientes bloques disponibles.");
+            return -1;
+        }
+
         int primerBloque = -1, anterior = -1;
 
         for (int i = 0; i < bloques.length && cantidad > 0; i++) {
@@ -37,6 +43,7 @@ public class Disco {
                 anterior = i;
                 ocupado[i] = true;
                 cantidad--;
+                bloquesLibres--;
             }
         }
 
@@ -44,21 +51,21 @@ public class Disco {
     }
 
     public void liberarBloques(int primerBloque) {
+        if (primerBloque < 0 || primerBloque >= bloques.length || !ocupado[primerBloque]) {
+            System.out.println("Error: Intentando liberar un bloque inválido o ya libre.");
+            return;
+        }
+
         Bloque actual = bloques[primerBloque];
 
-        while (actual != null) {
+        while (actual != null && ocupado[actual.getId()]) {
             ocupado[actual.getId()] = false;
+            bloquesLibres++;
             actual = actual.getSiguiente();
         }
     }
 
     public boolean hayEspacio(int cantidad) {
-        int libres = 0;
-        for (boolean b : ocupado) {
-            if (!b) {
-                libres++;
-            }
-        }
-        return libres >= cantidad;
+        return cantidad <= bloquesLibres;
     }
 }
